545cd8470ff51baefd4426a19da6d4c90ae3a321
Alex Tan <tanalex1@gmail.com>
2015-11-01 13:49:45 -0800

    Change oundbytes to soundbits, added getSoundById

--- tuneline/www/js/components/Database.jsx
+++ tuneline/www/js/components/Database.jsx
@@ -4,14 +4,31 @@ var db;
 
 module.exports = {
 
+	// Returns all sounds to the callback function
+	getSounds: function(callback) {
+		db.transaction(function(tx){getSounds(tx, callback)}, txErrorCB, txSuccessCB);
+	},
+
+	// Returns sound with given id to the callback function
+	// id is either an integer or a list of integers
+	getSoundById: function(callback, id) {
+		if (id.constructor === Array) {
+			// is array
+			console.log("Soundbit IDs requested: %o", id);
+		} else if ()
+		db.transaction(function(tx){getSounds(tx, callback)}, txErrorCB, txSuccessCB);
+	},
+
 	saveSound: function(jsonObj) {
 		db.transaction(
 			function(tx) {
-				tx.executeSql("INSERT INTO 'Soundbytes' (type,name,datetime,filename,url,tags,photo,author,location) VALUES (?,?,?,?,?,?,?,?,?)",[jsonObj.type, jsonObj.name, jsonObj.datetime, jsonObj.filename, jsonObj.url, jsonObj.tags, jsonObj.photo, jsonObj.author, JSON.stringify(jsonObj.location)], sqlSuccessCB, sqlErrorCB)}, txErrorCB, txSuccessCB);
+				tx.executeSql("INSERT INTO 'Soundbits' (type,name,datetime,filename,url,tags,photo,author,location) VALUES (?,?,?,?,?,?,?,?,?)",[jsonObj.type, jsonObj.name, jsonObj.datetime, jsonObj.filename, jsonObj.url, jsonObj.tags, jsonObj.photo, jsonObj.author, JSON.stringify(jsonObj.location)], sqlSuccessCB, sqlErrorCB)}, txErrorCB, txSuccessCB);
 	},
 
-	getSounds: function(callback) {
-		db.transaction(function(tx){getSounds(tx, callback)}, txErrorCB, txSuccessCB);
+	updateSound: function(jsonObj) {
+		db.transaction(
+			function(tx) {
+				tx.executeSql("UPDATE 'Soundbits' SET name=?, datetime=?,tags=?,photo=?,location=?) WHERE id=?",[jsonObj.name, jsonObj.datetime, jsonObj.tags, jsonObj.photo, JSON.stringify(jsonObj.location), jsonObj.id], sqlSuccessCB, sqlErrorCB)}, txErrorCB, txSuccessCB);	
 	}
 
 }
@@ -31,7 +48,7 @@ var getSounds = function(tx, callback) {
 		console.log("Rows in successCB: %o", rows);
 		callback(rows);
 	}
-	tx.executeSql("SELECT * FROM Soundbytes ORDER BY datetime DESC", [], function(tx,r){successCB(tx,r,callback)}, sqlErrorCB);
+	tx.executeSql("SELECT * FROM Soundbits ORDER BY datetime DESC", [], function(tx,r){successCB(tx,r,callback)}, sqlErrorCB);
 }
 
 // Takes SQLResultSet, returns rows as a list of JSON objects
@@ -49,7 +66,7 @@ var resultSetToList = function(results) {
 
 // Create the table if not present
 var createTable = function(tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS "Soundbytes" ("id" INTEGER PRIMARY KEY,"type" TEXT NOT NULL,"name" TEXT,"datetime" TEXT NOT NULL,"filename" TEXT,"url" INTEGER,"tags" TEXT,"photo" TEXT,"author" TEXT,"location" TEXT NOT NULL);');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS "Soundbits" ("id" INTEGER PRIMARY KEY,"type" TEXT NOT NULL,"name" TEXT,"datetime" TEXT NOT NULL,"filename" TEXT,"url" INTEGER,"tags" TEXT,"photo" TEXT,"author" TEXT,"location" TEXT NOT NULL);');
 }
 
 // Generic SQL error callback

53631460726ed702a3fc115ccf9876351dd01094
Alex Tan <tanalex1@gmail.com>
2015-10-31 17:50:50 -0700

    Fix saveSound object not being passed down properly

--- tuneline/www/js/components/Database.jsx
+++ tuneline/www/js/components/Database.jsx
@@ -6,7 +6,7 @@ module.exports = {
 
 	saveSound: function(jsonObj) {
 		db.transaction(
-			function(tx,jsonObj) {
+			function(tx) {
 				tx.executeSql("INSERT INTO 'Soundbytes' (type,name,datetime,filename,url,tags,photo,author,location) VALUES (?,?,?,?,?,?,?,?,?)",[jsonObj.type, jsonObj.name, jsonObj.datetime, jsonObj.filename, jsonObj.url, jsonObj.tags, jsonObj.photo, jsonObj.author, JSON.stringify(jsonObj.location)], sqlSuccessCB, sqlErrorCB)}, txSuccessCB, txErrorCB);
 	},
 

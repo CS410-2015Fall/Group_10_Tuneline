15ca17582a090c9f6956e9cb7ae9019391e7b8bd
bnguyen86 <bnguyen@outlook.com>
2015-11-01 17:22:42 -0800

    added location information getter

--- tuneline/www/js/components/MetaDataForm.jsx
+++ tuneline/www/js/components/MetaDataForm.jsx
@@ -12,7 +12,24 @@ const Database = require('./Database');
 
 var MetaDataForm = React.createClass({
 	getInitialState: function() {
+		this.getLocation();
 		return {value:0};
+	},
+	getLocation: function(callbackFunction){
+		navigator.geolocation.getCurrentPosition(function(position){
+			if(position){
+				this.currentPosition={
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+					accuracy: position.coords.accuracy,
+					altitude: position.coords.altitude,
+					name: 'default location name'
+				};
+			}
+			if(callbackFunction){
+				callbackFunction();
+			}
+		}.bind(this));
 	},
 	updateTime: function(event, value){
 		this.setState({
@@ -33,7 +50,17 @@ var MetaDataForm = React.createClass({
 		    	destinationType: Camera.DestinationType.DATA_URL
 			});
 	},
+	currentPosition:null,
 	save: function(event){
+		if(this.currentPosition){
+			var soundBite = this.createSoundbite();
+			Database.saveSound(soundBite);
+			this.props.callbackParent(0);
+		} else{
+			this.getLocation(this.save);
+		}		
+	},
+	createSoundbite:function(){
 		var soundBiteDate = new Date(this.refs.date.getValue());
 			soundBiteDate.setTime(this.refs.time.getTime());
 		var soundBite = {
@@ -45,16 +72,9 @@ var MetaDataForm = React.createClass({
 			tags: this.refs.tags.getValue(),
 			photo: 'placeholder photo',
 			author: 'some author id',
-			location: {
-				lat: 49.2602007,
-				lng: -123.2501255,
-				accuracy: 5,
-				altitude: 0,
-				name: 'location name'
-			}
+			location: this.currentPosition
 		};
-		Database.saveSound(soundBite);
-		this.props.callbackParent(0);
+
 		return soundBite;
 	},
 	render() {

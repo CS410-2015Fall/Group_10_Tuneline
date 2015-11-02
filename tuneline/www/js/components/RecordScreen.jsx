5523e3edf93c3c1762e009b25121aafce9a97229
bnguyen86 <bnguyen@outlook.com>
2015-11-02 04:25:01 -0800

    Changed record directory for iOS

--- tuneline/www/js/components/RecordScreen.jsx
+++ tuneline/www/js/components/RecordScreen.jsx
@@ -54,7 +54,7 @@ var RecordButton = React.createClass({
 			fileName = cordova.file.externalApplicationStorageDirectory + 
 							'files/' + fileName + '.m4a';
 		} else if(device.platform === 'iOS'){
-			fileName = cordova.file.documentsDirectory+fileName + '.wav';
+			fileName = cordova.file.applicationStorageDirectory+'Documents/'+fileName + '.wav';
 		} else{
 			fileName = fileName + '.wav';
 		}

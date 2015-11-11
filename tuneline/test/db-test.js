4a3426293ee3b1fda3ab5c79880eea0b8901891f
Alex Tan <tanalex1@gmail.com>
2015-11-10 18:39:54 -0800

    Start testing

---
+++ tuneline/test/db-test.js
@@ -0,0 +1,34 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var db = require('../Database.jsx');
+
+describe('test', function () {
+	it('should add 1+1 correctly', function (done) {
+		var onePlusOne = 1 + 1;
+		assert.equal(onePlusOne, 2);
+		// must call done() so that mocha know that we are... done.
+		// Useful for async tests.
+		done();
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+// functions to test:
+
+// saving soundbites to database
+// getting soundbites from database
+// getting a soundbite by id
+// update a soundbite
+// reset the table
+
+// *** get filtered sounds
--- tuneline/www/js/components/Database.jsx
+++ tuneline/www/js/components/Database.jsx
@@ -8,6 +8,12 @@ module.exports = {
 	// TODO: 	getSoundsByLocation?
 	//			getSoundsByPlaylist
 	//			getSoundsByFilter
+	//			getPlaylines - returns all playlists+names
+	//			updatePlayline
+	//			addToPlayline
+	//			removeFromPlayline
+	//			addFilter
+	//			removeFilter
 
 
 	// Returns all sounds to the callback function
@@ -15,10 +21,11 @@ module.exports = {
 		db.transaction(function(tx){getSounds(tx, callback)}, txErrorCB, txSuccessCB);
 	},
 
+	// TODO
 	// Returns all sounds which fall on and between the start/end dates
-	// start: 	must be defined (format: YYYY-MM-DD)
-	getSoundsByDate: function(callback, start, end) {
-		db.transaction(function(tx){getSoundsByDate(tx, callback, start, end)}, txErrorCB, txSuccessCB);
+	// filterObj: 	JSON object with filter parameters
+	getSoundsByFilter: function(callback, filterObj) {
+		db.transaction(function(tx){getSoundsByFilter(tx, callback, filterObj)}, txErrorCB, txSuccessCB);
 	},
 
 	// Returns sound with given id to the callback function
@@ -27,25 +34,9 @@ module.exports = {
 		db.transaction(function(tx){getSoundsById(tx, callback, id)}, txErrorCB, txSuccessCB);
 	},
 
-	// getSoundsByLocation: function(callback, lat, long) {
-
-	// },
-
-	// Returns sounds with the given searchString
-	// 	type: 	what is the search type? 
-	//			"tags","name","both"
-	//	searchString: 	the string to use as search parameter
-	getSoundsBySearch: function(callback, type, searchString) {
-		db.transaction(function(tx){getSoundsBySearch(tx, callback, type, searchString)}, txErrorCB, txSuccessCB);
-	},
-
+	// TODO: parse timestamp for DayOfWeek, TimeOfDay
 	saveSound: function(jsonObj) {
-		var timestamp = Math.round(jsonObj.datetime.getTime()/1000);
-		console.log("Timestamp: " + timestamp);
-		db.transaction(
-			function(tx) {
-				tx.executeSql("INSERT INTO 'Soundbites' (type,name,datetime,filename,url,tags,photo,author,location) VALUES (?,?,?,?,?,?,?,?,?)",[jsonObj.type, jsonObj.name, timestamp, jsonObj.filename, jsonObj.url, jsonObj.tags, jsonObj.photo, jsonObj.author, JSON.stringify(jsonObj.location)], sqlSuccessCB, sqlErrorCB)}, txErrorCB, txSuccessCB);
-
+		db.transaction(function(tx){saveSound(tx, jsonObj)}, txErrorCB, txSuccessCB);
 	},
 
 	updateSound: function(jsonObj) {
@@ -77,8 +68,38 @@ var getSounds = function(tx, callback) {
 	tx.executeSql("SELECT * FROM Soundbites ORDER BY datetime DESC", [], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
 }
 
-// TODO
-var getSoundsByDate = function(tx, callback, start, end) {
+var getSoundsByFilter = function(tx, callback, filterObj) {
+	var startDate;
+	var endDate;
+	var days;
+	var times;
+	var minLength;
+	var maxLength;
+	var hasPhoto;
+	var author;
+	var name;
+
+	// build the query with if (!== undefined) && (!= "")
+}
+
+var getSoundsById = function(tx, callback, id) {
+	if (isInt(id)) {
+		console.log("Soundbite ID requested: " + id);
+		tx.executeSql("SELECT * FROM Soundbites WHERE id=?", [id], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
+	} else if (id.constructor === Array) {
+		// is array
+		console.log("Soundbite IDs requested: " + id.join());
+		tx.executeSql("SELECT * FROM Soundbites WHERE id IN ('?')", [id.join()], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
+	}
+}
+
+// Takes:
+// 	start: 	must be defined (format: YYYY-MM-DD)
+// 	end: 	optional (format: YYYY-MM-DD)
+// Returns:
+//	sql query statement
+// TODO: test
+var getDateQuery = function(start, end) {
 	var explodeStart = start.split("-");
 	var explodeEnd;
 	var startInSeconds = new Date(explodeStart[0], explodeStart[1], explodeStart[2]);
@@ -94,28 +115,40 @@ var getSoundsByDate = function(tx, callback, start, end) {
 		endInSeconds = startInSeconds+86399;
 	}
 
-	tx.executeSql("SELECT * FROM Soundbites ORDER BY datetime DESC WHERE datetime BETWEEN ? AND ?", [startInSeconds,endInSeconds], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
+	return "SELECT * FROM Soundbites ORDER BY datetime DESC WHERE datetime BETWEEN " + startInSeconds + " AND " + endInSeconds;
 }
 
-var getSoundsById = function(tx, callback, id) {
-	if (isInt(id)) {
-		console.log("Soundbite ID requested: " + id);
-		tx.executeSql("SELECT * FROM Soundbites WHERE id=?", [id], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
-	} else if (id.constructor === Array) {
-		// is array
-		console.log("Soundbite IDs requested: " + id.join());
-		tx.executeSql("SELECT * FROM Soundbites WHERE id IN ('?')", [id.join()], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
+// TODO: test
+var getTagQuery = function(tags) {
+	// remove hastags, keep @
+	tags.replace("#","");
+	var tagsArray = tags.split(" ");
+	var query;
+
+	query = "SELECT * FROM Soundbites ORDER BY datetime DESC WHERE ";
+
+	var len = tagsArray.length;
+	for (var i=0; i<len; i++) {
+		if (i>=1) query+= " AND ";
+
+		query += "(tags LIKE %" + searchArray[i] + "%)"
 	}
+
+	return query;
 }
 
-// TODO
-var getSoundsBySearch = function(tx, callback, type, searchString) {
-	
-	
-	tx.executeSql("SELECT * FROM Soundbites ORDER BY datetime DESC", [], function(tx,r){soundQuerySuccessCB(tx,r,callback)}, sqlErrorCB);
+// TODO: make sure the engine provides length variable in jsonObj
+var saveSound(tx, jsonObj) {
+	// length is/should be provided
+
+	// change datetime to unix timestamp, get day of the week
+	var datetime = jsonObj.datetime;
+	var day = datetime.getDay();
+	var timestamp = Math.round(jsonObj.datetime.getTime()/1000);
+	console.log("Timestamp: " + timestamp);
+
+	tx.executeSql("INSERT INTO 'Soundbites' (type,name,datetime,filename,url,tags,photo,author,location,dayofweek) VALUES (?,?,?,?,?,?,?,?,?,?)",[jsonObj.type, jsonObj.name, timestamp, jsonObj.filename, jsonObj.url, jsonObj.tags, jsonObj.photo, jsonObj.author, JSON.stringify(jsonObj.location), day], sqlSuccessCB, sqlErrorCB)
 }
-
-
 
 
 // returns true if value is an int or a string representation of an int
@@ -129,24 +162,29 @@ var isInt = function(value) {
 }
 
 // Takes SQLResultSet, returns rows as a list of JSON objects
-var resultSetToList = function(results) {
+var resultSetToList = function(results, type) {
 	var len = results.rows.length;
 	var rows = [];
 
 	// loops through resultSet, parses serialized JSON location, parses timestamp back to javascript Date object
-	for (var i=0; i<len; i++){
+	for (var i=0; i<len; i++) {
     	rows.push(results.rows.item(i));
-    	var locObj = JSON.parse(results.rows.item(i).location);
-    	rows[rows.length-1].location = locObj;
-    	var datetimeObj = new Date(results.rows.item(i).datetime*1000);
-    	rows[rows.length-1].datetime = datetimeObj;
+    	if ((typeof type !== "underfined") && (type == "soundbite")) {
+	    	var locObj = JSON.parse(results.rows.item(i).location);
+	    	rows[rows.length-1].location = locObj;
+	    	var datetimeObj = new Date(results.rows.item(i).datetime*1000);
+	    	rows[rows.length-1].datetime = datetimeObj;
+	    }
     }
     return rows;
 }
 
 // Create the table if not present
 var createTables = function(tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS "Soundbites" ("id" INTEGER PRIMARY KEY,"type" TEXT NOT NULL,"name" TEXT,"datetime" INTEGER NOT NULL,"filename" TEXT,"url" INTEGER,"tags" TEXT,"photo" TEXT,"author" TEXT,"location" TEXT NOT NULL);');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS "Soundbites" ("id" INTEGER PRIMARY KEY,"type" TEXT NOT NULL,"name" TEXT,"datetime" INTEGER NOT NULL,"filename" TEXT,"url" INTEGER,"tags" TEXT,"photo" TEXT,"author" TEXT,"location" TEXT NOT NULL,"dayofweek" INTEGER NOT NULL);');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS "Playlists" ("pid" INTEGER PRIMARY KEY,"name" TEXT)');
+	// tx.executeSql('CREATE TABLE IF NOT EXISTS "Filters" ()');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS "SoundbitesPlaylistMap" ("sid" INTEGER NOT NULL, "pid" INTEGER NOT NULL, FOREIGN KEY(sid) REFERENCES Soundbites(id), FOREGIN KEY(pid) REFERENCES Playlist(pid))');
 	if (prepop) {
 		populate(tx);
 	}
@@ -160,7 +198,7 @@ var populate = function(tx) {
 }
 
 var soundQuerySuccessCB = function(tx, queryResult, callback) {
-	var rows = resultSetToList(queryResult);
+	var rows = resultSetToList(queryResult, "soundbite");
 	console.log("Rows in soundQuerySuccessCB: " + rows.join());
 	callback(rows);
 }

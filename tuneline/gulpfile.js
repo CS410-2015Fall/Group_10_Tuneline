af1b78eae04f05c1f04c9f6250ddca013d36523f
Alex Tan <tanalex1@gmail.com>
2015-11-13 12:46:13 -0800

    Comment out lots of gulpfile.js

--- tuneline/gulpfile.js
+++ tuneline/gulpfile.js
@@ -1,78 +1,78 @@
 var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var webpack = require('webpack-stream');
-var staticHash = require('gulp-static-hash');
-var uglify = require('gulp-uglify');
-var cordova = require('cordova');
-var rimraf = require('rimraf');
-var runSequence = require('run-sequence');
+// var livereload = require('gulp-livereload');
+// var webpack = require('webpack-stream');
+// var staticHash = require('gulp-static-hash');
+// var uglify = require('gulp-uglify');
+// var cordova = require('cordova');
+// var rimraf = require('rimraf');
+// var runSequence = require('run-sequence');
 
 // testing
 var browserSync = require("browser-sync"),
     browserify = require("browserify"),
     source = require("vinyl-source-stream");
 
-var tasks = {
-  webpack: function() {
-    return gulp.src('./www/js/index.jsx')
-      .pipe(webpack(require('./webpack.config')))
-      .pipe(gulp.dest('./www/js/'))
-  }
-}
+// var tasks = {
+//   webpack: function() {
+//     return gulp.src('./www/js/index.jsx')
+//       .pipe(webpack(require('./webpack.config')))
+//       .pipe(gulp.dest('./www/js/'))
+//   }
+// }
 
-gulp.task('webpack', tasks.webpack)
+// gulp.task('webpack', tasks.webpack)
 
-gulp.task('livereload', function() {
-  var stream = tasks.webpack()
-  var platforms = require('./platforms/platforms.json')
-  for (var platform in platforms) {
-    stream = stream.pipe(gulp.dest('./platforms/' + platform + '/www/js'))
-  }
-  return stream.pipe(livereload())
-})
+// gulp.task('livereload', function() {
+//   var stream = tasks.webpack()
+//   var platforms = require('./platforms/platforms.json')
+//   for (var platform in platforms) {
+//     stream = stream.pipe(gulp.dest('./platforms/' + platform + '/www/js'))
+//   }
+//   return stream.pipe(livereload())
+// })
 
-gulp.task('watch', ['serve'], function() {
-  livereload.listen({port: 35729})
-  gulp.watch('./www/js/**/*.jsx', ['livereload'])
-})
+// gulp.task('watch', ['serve'], function() {
+//   livereload.listen({port: 35729})
+//   gulp.watch('./www/js/**/*.jsx', ['livereload'])
+// })
 
-gulp.task('build', function(cb) {
-    return runSequence('webpack', 'uglify', 'hash')
-})
+// gulp.task('build', function(cb) {
+//     return runSequence('webpack', 'uglify', 'hash')
+// })
 
-gulp.task('prepare', function(cb) {
-    return cordova.prepare(cb)
-})
+// gulp.task('prepare', function(cb) {
+//     return cordova.prepare(cb)
+// })
 
-gulp.task('serve', function(cb) {
-    return cordova.serve(cb)
-})
+// gulp.task('serve', function(cb) {
+//     return cordova.serve(cb)
+// })
 
-gulp.task('hash', function() {
-    return gulp.src(['www/*.html'])
-        .pipe(staticHash({asset: 'www/js'}))
-        .pipe(gulp.dest('www'));
-});
+// gulp.task('hash', function() {
+//     return gulp.src(['www/*.html'])
+//         .pipe(staticHash({asset: 'www/js'}))
+//         .pipe(gulp.dest('www'));
+// });
 
-gulp.task('uglify', function () {
-    return gulp.src(['www/js/**/*.js'])
-        .pipe(uglify({
-            compress: {
-                global_defs: {
-                    DEBUG: false
-                }
-            }
-        }))
-        .pipe(gulp.dest('www/js'));
-});
+// gulp.task('uglify', function () {
+//     return gulp.src(['www/js/**/*.js'])
+//         .pipe(uglify({
+//             compress: {
+//                 global_defs: {
+//                     DEBUG: false
+//                 }
+//             }
+//         }))
+//         .pipe(gulp.dest('www/js'));
+// });
 
-gulp.task('clean', function(cb) {
-    return rimraf('./platforms/**/www/js/**/*.jsx', cb)
-})
+// gulp.task('clean', function(cb) {
+//     return rimraf('./platforms/**/www/js/**/*.jsx', cb)
+// })
 
-gulp.task('install', function(cb) {
-    return cordova.platform('add', ['browser', 'ios', 'android'], cb)
-})
+// gulp.task('install', function(cb) {
+//     return cordova.platform('add', ['browser', 'ios', 'android'], cb)
+// })
 
 // testing
 

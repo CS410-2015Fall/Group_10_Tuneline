b72f727aeea10c7fc1f965cb58e2e510c292bb20
JonMercer <mercer.jon@gmail.com>
2015-11-21 15:58:25 -0800

    Removed /tuneline and set up saveService

---
+++ www/js/saveService.js
@@ -0,0 +1,99 @@
+angular.module('saveService', [])
+
+  /*
+  * Goal: given a metadata file, find the filename, move the file, update the filename, and push to DB
+  * */
+
+
+  .factory('SaveService', function($cordovaFile) {
+    var mediaObject;
+    var mediaLength;
+    var mediaStatus = 0;
+    return {
+      initPlayer: function(fileName){
+        mediaObject = $cordovaMedia.newMedia(fileName);
+        if(mediaObject){
+          mediaObject.play();
+          mediaObject.stop();
+        }
+        mediaControllerRepeater = window.setInterval(function(){
+
+          var duration = mediaObject.getDuration();
+          if(duration > 0){
+            mediaLength = duration;
+            window.clearInterval(mediaControllerRepeater);
+          }
+        },100);
+
+      },
+
+      // Media.MEDIA_NONE = 0;
+      // Media.MEDIA_STARTING = 1;
+      // Media.MEDIA_RUNNING = 2;
+      // Media.MEDIA_PAUSED = 3;
+      // Media.MEDIA_STOPPED = 4;
+
+      startRecording: function(fileName){
+        mediaObject = new Media(fileName,function(){
+          console.log('*#*#*#*#*#mediaObject SUCCESS: '+ status);
+        }, function(){
+          console.log('*#*#*#*#*#mediaObject STATUS (record): '+ status);
+        }, function(status){
+          console.log('*#*#*#*#*#mediaObject STATUS (record): '+ status);
+          mediaStatus = status;
+        });
+        mediaObject.startRecord();
+      },
+
+      stopRecording: function(mediaObject){
+        if(mediaObject){
+          mediaObject.stopRecord();
+          mediaObject.release();
+          mediaObject = null;
+        }
+      },
+
+      playMedia:  function(fileName){
+        if(mediaObject && mediaStatus !== 0){
+          mediaObject.stop();
+        }
+        mediaObject = new Media(fileName,function(){
+
+        }, function(){
+
+        }, function(status){
+          console.log('*#*#*#*#*#mediaObject STATUS (record): '+ status);
+          mediaStatus = status;
+        });
+        mediaObject.play();
+      },
+
+      pauseMedia:  function(){
+        if(mediaObject){
+          mediaObject.pause();
+        }
+      },
+
+      stopMedia:  function(){
+        if(mediaObject){
+          mediaObject.stop();
+        }
+      },
+
+      resetMedia: function(){
+        if(mediaObject){
+          mediaObject.release();
+        }
+      },
+
+      seekTo: function(position){
+        if(mediaObject){
+          mediaObject.seekTo(position);
+        }
+      },
+
+      getMediaStatus: function(){
+        return mediaStatus;
+      }
+    };
+  });

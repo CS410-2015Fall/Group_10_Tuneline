51f93acb835bf9bba96b728fd46697fd269b4490
JonMercer <mercer.jon@gmail.com>
2015-11-21 17:52:54 -0800

    saveService.saveMeta implemented but not tested

--- www/js/saveService.js
+++ www/js/saveService.js
@@ -5,7 +5,7 @@ angular.module('saveService', [])
   * */
 
 
-  .factory('SaveService', function($cordovaFile) {
+  .factory('SaveService', function($cordovaFile, databaseService) {
     var mediaObject;
     var mediaLength;
     var mediaStatus = 0;
@@ -94,6 +94,50 @@ angular.module('saveService', [])
 
       getMediaStatus: function(){
         return mediaStatus;
+      },
+
+      saveMeta: function(soundbite){
+
+        //soundbite = {
+        //  id: null,
+        //  type: 'default',
+        //  name: '',
+        //  datetime: null,
+        //  filename: scope.filename,
+        //  length: null,
+        //  url: null,
+        //  tags: null,
+        //  photo: '',
+        //  author: 'some author id',
+        //  location: null
+        //};
+
+        var fileName = soundbite.fileName;
+        var platform = $cordovaDevice.getPlatform()
+
+
+        if(platform === 'Android'){
+          $cordovaFile.moveFile(cordova.file.cache,
+                                'files/' + fileName + '.m4a',
+                                cordova.file.externalApplicationStorageDirectory,
+                                'files/' + fileName + '.m4a');
+          $cordovaFile.removeFile(cordova.file.cache, 'files/' + fileName + '.m4a');
+
+        } else if(platform === 'iOS'){
+          $cordovaFile.moveFile(cordova.file.cacheDirectory,
+                                '/' + fileName + '.wav',
+                                cordova.file.dataDirectory,
+                                '/' + fileName + '.wav');
+          $cordovaFile.removeFile(cordova.file.cacheDirectory, '/' + fileName + '.m4a');
+
+        } else{
+          fileName = fileName + '.wav';
+        }
+
+        soundbite.filename = fileName;
+
+        databaseService.saveSound(soundbite);
+
       }
     };
   });
